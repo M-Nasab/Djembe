048a8fc857b95d6673d80b0cc8f63131fd0c2792
Mostafa Amininasab <m.amininasab@alibaba.ir>
2021-03-23 14:13:40 +0430

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
+import { terser } from "rollup-plugin-terser";
+import filesize from 'rollup-plugin-filesize';
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const name = "Djembe";
+
+const destBase = "dist/bundle";
+const destExtension = `${isProduction ? ".min" : ""}.js`;
+
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            file: `${destBase}.esm${destExtension}`,
+            format: "esm"
+        },
+        {
+            file: `${destBase}.umd${destExtension}`,
+            format: "umd",
+            name,
+        },
+    ],
+    plugins: [
+        commonjs(),
+        nodeResolve(),
+        babel({
+            babelHelpers: 'bundled',
+            exclude: 'node_modules/**'
+        }),
+        isProduction && terser(),
+        filesize()
+    ].filter(Boolean),
+};
